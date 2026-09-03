448062e61c696de931156b4f57a2397f7870a335
amanda.weiler <amanda.weiler@T20019313085877>
2026-09-03 10:38:37 -0300

    Adiciona exercício 6 de vetores

---
+++ AppConsoleExercicio6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Declaração de variáveis
+int[] numeros = new int[10];
+for (int i = 0; i < numeros.Length; i++) 
+{
+    Console.WriteLine($"Informe o {i + 1} número");
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Vetor na ordem original");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"{numeros[i]}");
+}
+Console.WriteLine("");
+Console.WriteLine($"Vetor na ordem inversa");
+for (int i = 9; i >=0; i--)
+{
+    Console.WriteLine($"{numeros[i]}");
+}
